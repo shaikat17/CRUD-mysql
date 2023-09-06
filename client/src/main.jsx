ae7b2a3e8c3b868240698ca84ba7cac286d6c05d
shaikat17 <shaikatpal56@gmail.com>
2023-09-07 00:58:07 +0600

    Different pages added for client site.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Books from './pages/Books';
+import Add from './pages/Add';
+import Update from './pages/Update';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Books />,
+  },
+  {
+    path: "/add",
+    element: <Add />,
+  },
+  {
+    path: "/update",
+    element: <Update />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
