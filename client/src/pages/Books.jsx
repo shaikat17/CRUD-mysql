28ae8a3425b5e0db86c47e10db4f2db881264c0c
shaikat17 <shaikatpal56@gmail.com>
2023-09-07 01:07:51 +0600

    Getting data in client site.

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Books = () => {
+    const [books, setBooks] = useState([])
+
+    useEffect(() => {
+        const fetchAllBooks = async () => {
+            try {
+            const res = await axios("http://localhost:5000/books")
+            setBooks(res.data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchAllBooks()
+    }, [])
+
     return (
         <div>
-            Books
+            <h1>Shaikat Book Shop</h1>
+            <div className="books">
+                {books.map(book => {
+                    return (
+                        <div className="book" key={book.id}>
+                            {book.cover && <img src={book.cover} alt={book.name} /> }
+                            <h2>{book.name}</h2>
+                            <p>{book.desc}</p>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     );
 };
