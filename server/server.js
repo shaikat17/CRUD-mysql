82fdaea0ee171d7583a6a502e679f19c9ac3b187
shaikat17 <shaikatpal56@gmail.com>
2023-09-07 00:36:56 +0600

    Successfully handle get and post request.

--- server/server.js
+++ server/server.js
@@ -22,6 +22,16 @@ app.get('/books', (req,res) => {
     })
 })
 
+app.post('/books', (req, res) => {
+    const q = "INSERT INTO books (`name`, `desc`, `cover`) VALUES(?)"
+    const values = ["name from backend", "description from backend", "cover from backend"]
+
+    db.query(q, [values], (err, data) => {
+        if(err) return res.json(err)
+        return res.json(data)
+    })
+})
+
 app.listen(5000, () => {
     console.log("Server start")
 })
